f4bbb22b8bfbf07ec2a583d5d74d847dc37d51c6
jazzy <minh15553@gmail.com>
2021-12-11 00:38:02 +0700

    fix cart-drawer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="home">
-            Home
+            <div className="home-container container">
+                <div className="home-inner">111111111</div>
+                <div className="home-inner">111111111</div>
+                <div className="home-inner">11111111</div>
+                <div className="home-inner">1111111111</div>
+            </div>
         </div>
     )
 }
