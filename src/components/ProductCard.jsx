bc2b0f2953826664a4517372b7794d9577ddffc5
jazzy <minh15553@gmail.com>
2022-04-02 21:37:39 +0700

    fix: product modal transition

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import { useState } from "react";
 import { useProducts } from "./context/productsContext";
 import ProductModal from "./ProductModal";
+import {CSSTransition} from "react-transition-group";
 
 const ProductCard = ({ item }) => {
-    const [showModal, setShowModal] = useState(false)
-    const {toggleFavorite} = useProducts()
+    const [showModal, setShowModal] = useState(false);
+    const { toggleFavorite } = useProducts();
 
     const handleClose = () => {
-        setShowModal(false)
-    }
+        setShowModal(false);
+    };
 
     return (
         <>
             <div className="product__list__item">
                 <div className="product__list__item__image">
                     <img src={item.image01} alt="" />
-                    <button className="btn btn-white btn-view" onClick={() => setShowModal(true)}>
+                    <button
+                        className="btn btn-white btn-view"
+                        onClick={() => setShowModal(true)}
+                    >
                         Quick View
                     </button>
                 </div>
@@ -25,14 +29,27 @@ const ProductCard = ({ item }) => {
                         <p>{item.title}</p>
                         <span>{item.price} VND</span>
                     </div>
-                    <div className="product__list__item__content__right" onClick={() => {toggleFavorite(item.id)}}>
+                    <div
+                        className="product__list__item__content__right"
+                        onClick={() => {
+                            toggleFavorite(item.id);
+                        }}
+                    >
                         <i
-                            className={`bx bxs-heart favorite-${item.isFavorite ? 'red' : 'gray'}`}
+                            className={`bx bxs-heart favorite-${
+                                item.isFavorite ? "red" : "gray"
+                            }`}
                         ></i>
                     </div>
                 </div>
             </div>
-            {showModal && <ProductModal open={showModal} handleClose={handleClose} item={item} />}
+            <CSSTransition in={showModal} timeout={250} unmountOnExit>
+                <ProductModal
+                    open={showModal}
+                    handleClose={handleClose}
+                    item={item}
+                />
+            </CSSTransition>
         </>
     );
 };
